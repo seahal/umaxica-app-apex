a2aa82996fd68b862eeb08c8d492aedf98ef9970
seahal <m.shiihara@gmail.com>
2025-11-03 22:29:48 +0900

    [update] set up!

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -1,7 +1,15 @@
-import { cloudflare } from '@cloudflare/vite-plugin'
-import { defineConfig } from 'vite'
-import ssrPlugin from 'vite-ssr-components/plugin'
+import { cloudflare } from "@cloudflare/vite-plugin";
+import { defineConfig } from "vite";
+import ssrPlugin from "vite-ssr-components/plugin";
 
 export default defineConfig({
-  plugins: [cloudflare(), ssrPlugin()]
-})
+	plugins: [cloudflare(), ssrPlugin()],
+	server: {
+		host: true,
+		port: 5071,
+		strictPort: true,
+		watch: {
+			usePolling: true,
+		},
+	},
+});
